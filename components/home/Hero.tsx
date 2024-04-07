1113f999627b348fc2adba40483d1a1ecef579ff
yamataku0518 <truepartner312@gmail.com>
2024-04-08 05:36:52 +0900

    update

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 const Hero = () => {
   return (
-    <div className="relative w-[1920px]">
-      <Image src="/img/hero-bg.png" alt="alt" width={1920} height={1024.66} className="" />
+    <div className="relative">
+      <Image src="/img/hero-bg.png" alt="alt" width={1920} height={1024.66} className="w-full" />
       <div className="absolute top-0 left-0 flex w-full">
         <div className="flex flex-col mx-auto">
           <h1 className=" text-white mt-[381.51px] mb-[48.65px]  border-white border-l-[8.41px] pl-[18.38px] font-[700] text-[72px] leading-[87.14px] tracking-[-1.7%] w-[1306.12px]">Kvalitní okna od výrobce Rehau v nejlepším poměru cena:výkon  </h1>
