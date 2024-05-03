cf4468964484aa59b7e326707c486fd279c912e3
yamataku0518 <truepartner312@gmail.com>
2024-05-04 01:53:17 +0900

    SEO

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,5 @@
 import { I18nProviderClient } from "@/locales/client";
 
-export const runtime = 'edge'
-
 export default function RootLayout({ params: { locale }, children }: { params: { locale: string }, children: React.ReactElement }) {
   return (
     <I18nProviderClient locale={locale}>
