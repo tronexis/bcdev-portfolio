d85be38e47cfcdab7c4842331b02657a45de2492
yamataku0518 <truepartner312@gmail.com>
2024-05-04 01:07:24 +0900

    project structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="theme init" src="/js/theme.js" />
       <body className={inter.className}>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
